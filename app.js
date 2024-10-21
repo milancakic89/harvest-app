086306201ddde195d3431b165acb7db7da1e8595
Milan Cakic <58387889+milancakic89@users.noreply.github.com>
2024-10-21 15:14:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set("views", "views");
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin, 'https://harvest-app-66x0.onrender.com/')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Autorization')
   next()
